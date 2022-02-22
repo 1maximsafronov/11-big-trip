20c13a1f4ac1551fab28674064988e8b4c6ef6f4
Maxim Safronov <1maximsafronov@gmail.com>
2022-02-22 17:10:53 +0700

    Добавляет отрисовку компонентов

--- src/main.js
+++ src/main.js
@@ -39,9 +39,6 @@ const createFilterTemplate = () => {
   )
 };
 
-const createSortTemplate = () => {
-
-};
 
 const createTripInfoTemplate = () => {
   return (
@@ -436,3 +433,11 @@ const createContentTemplate = () => {
 const pageBodyElement = document.querySelector(`.page-body`);
 const pageMainElement = pageBodyElement.querySelector(`.page-main`);
 const tripMainElement = pageBodyElement.querySelector(`.trip-main`);
+const tripControlsElement = pageBodyElement.querySelector(`.trip-controls`);
+const tripEventsElement = pageMainElement.querySelector(`.trip-events`);
+
+render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
+render(tripControlsElement, createSiteMenuTemplate(), `beforeEnd`);
+render(tripControlsElement, createFilterTemplate(), `beforeEnd`);
+render(tripEventsElement, createSortTemplate(), `beforeEnd`);
+render(tripEventsElement, createContentTemplate(), `beforeEnd`);
