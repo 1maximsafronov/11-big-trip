981d4794215505a593fdab21db257017a6688636
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-08 12:33:12 +0700

    W.I.P. Рефакторинг TripController

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -16,9 +16,9 @@ export default class Trip {
 
     this._pointController = {};
 
-    this._noEventsComponent = new NoEventsComponent();
-    this._sortComponent = new SortComponent();
-    this._tripDaysComponent = new TripDaysComponent();
+    this._noEventsComponent = null;
+    this._sortComponent = null;
+    this._tripDaysComponent = null;
 
     this._handleSortChange = this._handleSortChange.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
@@ -31,11 +31,37 @@ export default class Trip {
     this._renderTrip();
   }
 
-  _handleDataChange(updatetPoint) {
-    this._pointController[updatetPoint.id].init(updatetPoint, this._offers);
+  _renderTrip() {
+    if (this._getPoints().length <= 0) {
+      this._renderNoEvents();
+      return;
+    }
 
-    console.log(updatetPoint.isFavorite ? `Фаворит` : `Не фаворит`);
+    this._renderSort();
+    this._renderTripDays();
   }
+
+  _renderNoEvents() {
+    this._noEventsComponent = new NoEventsComponent();
+    render(this._container, this._noEventsComponent);
+  }
+
+  _renderSort() {
+    this._sortComponent = new SortComponent();
+    render(this._container, this._sortComponent);
+
+    this._sortComponent.setSortTypeChangeHandler(this._handleSortChange);
+  }
+
+  _renderTripDays() {
+    this._tripDaysComponent = new TripDaysComponent();
+    render(this._container, this._tripDaysComponent);
+
+    const eventsListElement = this._container.querySelector(`.trip-events__list`);
+
+    this._renderPoints(eventsListElement, this._getPoints());
+  }
+
 
   _renderPoint(container, point) {
     const pointController = new PointController(container, this._handleModeChange, this._handleDataChange);
@@ -48,10 +74,28 @@ export default class Trip {
     .forEach((point) => this._renderPoint(container, point));
   }
 
-  _handleModeChange() {
+
+  _clearPointsList() {
     Object
       .values(this._pointController)
-      .forEach((controller) => controller.resetView());
+      .forEach((controller) => controller.destroy());
+  }
+
+
+  _getPoints() {
+    const points = this._pointsModel.getPoints();
+
+    switch (this._currentSortType) {
+      case SortType.BY_PRICE:
+        points.sort((pointA, pointB) => pointB.basePrice - pointA.basePrice);
+        break;
+      case SortType.BY_TIME:
+        // сортировка point по времени
+        break;
+    }
+
+
+    return points;
   }
 
   _handleSortChange(sortType) {
@@ -68,49 +112,15 @@ export default class Trip {
     this._renderPoints(eventsListElement, this._getPoints());
   }
 
-  _renderSort() {
-    render(this._container, this._sortComponent);
+  _handleDataChange(updatetPoint) {
+    this._pointController[updatetPoint.id].init(updatetPoint, this._offers);
 
-    this._sortComponent.setSortTypeChangeHandler(this._handleSortChange);
+    console.log(updatetPoint.isFavorite ? `Фаворит` : `Не фаворит`);
   }
 
-  _renderNoEvents() {
-    render(this._container, this._noEventsComponent);
-  }
-
-  _clearPointsList() {
+  _handleModeChange() {
     Object
       .values(this._pointController)
-      .forEach((controller) => controller.destroy());
-  }
-
-  _renderTrip() {
-    if (this._getPoints().length <= 0) {
-      this._renderNoEvents();
-      return;
-    }
-
-    this._renderSort();
-    render(this._container, this._tripDaysComponent);
-
-    const eventsListElement = this._container.querySelector(`.trip-events__list`);
-
-    this._renderPoints(eventsListElement, this._getPoints());
-  }
-
-  _getPoints() {
-    const points = this._pointsModel.getPoints();
-
-    switch (this._currentSortType) {
-      case SortType.BY_PRICE:
-        points.sort((pointA, pointB) => pointB.basePrice - pointA.basePrice);
-        break;
-      case SortType.BY_TIME:
-        // сортировка point по времени
-        break;
-    }
-
-
-    return points;
+      .forEach((controller) => controller.resetView());
   }
 }
