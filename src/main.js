7be4c4eaef0fd9b731fd49fe75c562ecd8f67a62
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-18 01:32:55 +0700

    Рефакторинг main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import {render} from "./utils/render";
 // import {RenedrPosition} from "./const";
 import Api from "./api/api.js";
+import Store from "./api/store";
+import Provider from "./api/provider";
+
+import PointsModel from "./models/points";
+import OffersModel from "./models/offers";
+import DestinationsModel from "./models/destinations";
+
 import NewEventButtonComponent from "./components/new-event-button";
 import TripControlsComponent from "./components/trip-controls";
 import SiteMenuComponent from "./components/site-menu";
@@ -8,14 +15,6 @@ import TripInfoComponent from "./components/trip-info";
 import FilterComponent from "./components/filter";
 
 import TripController from "./controllers/trip";
-import PointsModel from "./models/points";
-import DestinationsModel from "./models/destinations";
-import OffersModel from "./models/offers";
-import Provider from "./api/provider";
-import Store from "./api/store";
-
-// import {generatePoints} from "./mock/event";
-// import {generateOffers} from "./mock/offer";
 
 const END_POINT = `https://15.ecmascript.pages.academy/big-trip`;
 const AUTH_TOKEN = `Basic 5oDAQcFxqGT1ZpU`;
@@ -24,39 +23,41 @@ const STORE_PREFIX = `big-trip-12-localStore`;
 const STORE_VERSION = `v1.0.0`;
 const STORE_NAME = `${STORE_PREFIX}-${STORE_VERSION}`;
 
-// const POINTS_COUNT = 10;
 const localStorage = window.localStorage;
 const api = new Api(END_POINT, AUTH_TOKEN);
 const store = new Store(STORE_NAME, localStorage);
 const apiWithProvider = new Provider(api, store);
+
 const pointsModel = new PointsModel();
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
-
-// const offers = generateOffers();
-// const points = generatePoints(POINTS_COUNT, offers);
 
 const pageBodyElement = document.querySelector(`.page-body`);
 const pageMainElement = pageBodyElement.querySelector(`.page-main`);
 const tripMainElement = pageBodyElement.querySelector(`.trip-main`);
 const tripEventsElement = pageMainElement.querySelector(`.trip-events`);
 
+// * Инициализация компонентов
+const tripInfoComponent = new TripInfoComponent();
 const tripControlsComponent = new TripControlsComponent();
+const newEventBtnComponent = new NewEventButtonComponent();
+const siteMenuComponent = new SiteMenuComponent();
+const filterComponent = new FilterComponent();
+
+// * Инициализация контроллеров
 const tripController = new TripController(tripEventsElement, pointsModel, offersModel, apiWithProvider);
 
+// * Рендеринг компоннетов
 render(tripMainElement, [
-  new TripInfoComponent(),
+  tripInfoComponent,
   tripControlsComponent,
-  new NewEventButtonComponent()
+  newEventBtnComponent
 ]);
 
 render(tripControlsComponent, [
-  new SiteMenuComponent(),
-  new FilterComponent()
+  siteMenuComponent,
+  filterComponent
 ]);
-
-// pointsModel.setPoints(points);
-// tripController.init(offers);
 
 const loadDestinations = () => {
   return apiWithProvider.getDestinations()
