74cb5d6a3a4a3c7794de61c956fd13778f898edf
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-18 10:58:05 +0700

    Рефакторинг site-menu

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,21 +1,24 @@
-import Abstract from "./abstract";
+import Abstract from "./smart";
 
 const ACTIVE_CLASS = `trip-tabs__btn--active`;
 
 export default class SiteMenu extends Abstract {
   constructor(activeTab) {
     super();
-
-    this._activeTab = activeTab;
+    this._data = {
+      activeTab
+    };
 
     this._changeTabHandler = this._changeTabHandler.bind(this);
   }
 
   _getTemplate() {
+    const {activeTab} = this._data;
+
     return (
       `<nav class="trip-controls__trip-tabs trip-tabs">
-        <a class="trip-tabs__btn ${this._activeTab === `table` ? ACTIVE_CLASS : ``}" href="#" data-menu-tab="table">Table</a>
-        <a class="trip-tabs__btn ${this._activeTab === `stats` ? ACTIVE_CLASS : ``}" href="#" data-menu-tab="stats">Stats</a>
+        <a class="trip-tabs__btn ${activeTab === `table` ? ACTIVE_CLASS : ``}" href="#" data-menu-tab="table">Table</a>
+        <a class="trip-tabs__btn ${activeTab === `stats` ? ACTIVE_CLASS : ``}" href="#" data-menu-tab="stats">Stats</a>
       </nav>`
     );
   }
@@ -24,8 +27,11 @@ export default class SiteMenu extends Abstract {
     if (evt.target.tagName === `A`) {
       evt.preventDefault();
       const newTab = evt.target.dataset.menuTab;
-      if (this._activeTab !== newTab) {
+      if (this._data.activeTab !== newTab) {
         this._callback.changeTab(evt.target.dataset.menuTab);
+        this.updateData({
+          activeTab: newTab
+        });
       }
     }
   }
@@ -34,5 +40,9 @@ export default class SiteMenu extends Abstract {
     this._callback.changeTab = cb;
 
     this.getElement().addEventListener(`click`, this._changeTabHandler);
+  }
+
+  restoreHandlers() {
+    this.setChangeTabHahndler(this._callback.changeTab);
   }
 }
